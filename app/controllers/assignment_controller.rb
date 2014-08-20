a11c5c49abba01385e073a776b05b85e002722b8
Ziad Abdo <mr.ziad.abdo@gmail.com>
2014-08-20 16:25:13 -0700

    *small refactor in assignment controller 	*added code to make sure that assignment would update 		slots availability based on the largest boat.

--- app/controllers/assignment_controller.rb
+++ app/controllers/assignment_controller.rb
@@ -1,9 +1,13 @@
 class AssignmentController < ApplicationController
 
 	def create
+			slot = Slot.find(params[:timeslot_id])
+			boat = Boat.find(params[:boat_id])
 
-		if (Slot.find(params[:timeslot_id]) && Boat.find(params[:boat_id]))
+		if (slot && boat)
 			Assignment.create(slot_id: params[:timeslot_id], boat_id: params[:boat_id])
+			slot.update_attributes(availability: boat.capacity) if (boat.capacity > slot.availability)
+
 		else
 			render json: "You have requested invalid boat or timeslot"
 		end
